879230627e2847f089acff9282df919d510cb920
Станислав <stanislav9234@gmail.com>
2025-02-26 12:03:38 +0300

    fix

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводятся три числа.Вывести большее их них.
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            switch (true)
+            {
+                case bool when a > b && a > c:
+                    Console.WriteLine("max = {0}", a);
+                    break;
+                case bool when b > c:
+                    Console.WriteLine("max = {0}", b);
+                    break;
+                default:
+                    Console.WriteLine("max = {0}", c);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
