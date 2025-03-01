4af811ea0f7c84f8f6036f54af7a2d982c630393
Станислав <stanislav9234@gmail.com>
2025-03-02 00:27:55 +0300

    ДЗ Task4

--- Task4_8/Program.cs
+++ Task4_8/Program.cs
@@ -1,38 +1,48 @@
-﻿ namespace Task4_8
+﻿namespace Task4_8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //yellow-star Вводится число. Вывести его в зеркальном виде. Количество цифр в числе заранее неизвестно. Не использовать строки
-            /*
-             Трехзначное число int a = input / 100; //сотни (первая цифра)
-            int b = input % 100/10; //десятые (вторая цифра)
-            int c = input % 10; //единицы (третья (последняя) цифра)
-            */
+            //yellow-star Вводится число. Вывести его в зеркальном виде. Количество цифр в числе заранее неизвестно. Не использовать строк
 
             Console.WriteLine("Введите число n");
             int n = Convert.ToInt32(Console.ReadLine()); //кол-во цифр в числе n = i
 
+            #region Находим кол-во цифр
             int res = n;
+            int k = 0;
+
+            while (res != 0)
+            {
+                res = res / 10;
+                k++;
+            }
+            #endregion
+
+            #region Отзеркаливаем
+
+            int a = 100;
             int i = 0;
-            if (res == 0)
-                Console.WriteLine("Количество цифр в числе = 1");
+
+            if (k == 1) //Получаем первую цифру
+            {
+                Console.WriteLine(n);
+            }
             else
             {
-                while (res != 0)
-                {
-                    res = res / 10;
-                    i++;
-                }
-                Console.WriteLine("Количество цифр в числе = {0}", i);
+                Console.Write(n % 10);
             }
-
-            Console.Write()
-
-
-
+            while (i < k)
+            {
+                if (n % a / (a / 10) == 0)
+                    break;
+                Console.Write(n % a / (a / 10));
+                i++;
+                a *= 10;
+            }
             Console.ReadKey();
+            #endregion
         }
     }
 }
