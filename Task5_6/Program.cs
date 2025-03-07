aedcfc4869149f5b03d1b5cda991059b78b305f7
Станислав <stanislav9234@gmail.com>
2025-03-07 18:29:22 +0300

    fix

--- Task5_6/Program.cs
+++ Task5_6/Program.cs
@@ -10,7 +10,27 @@
             //1 0 1 0 1
             //0 1 0 1 0
             //1 0 1 0 1
-            Console.WriteLine("Hello, World!");
+            const int n = 5;
+            int[,] array = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i % 2 == 0)//четные строки, включая ноль
+                    {
+                        array[i, j] = j % 2 == 0 ? 1 : 0; //четные столбцы, включая ноль
+                        Console.Write("{0} ", array[i, j]);
+                    }
+                    else ////нечетные строки
+                    {
+                        array[i, j] = j % 2 == 0 ? 0 : 1; //четные столбцы, включая ноль
+                        Console.Write("{0} ", array[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Task5_7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task5_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сформировать двумерный массив из 10 строк и 5 столбцов.
+            //Заполнить его случайными числами в диапазоне [0, 10].
+            //Определить максимальный элемент в каждой строке
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
