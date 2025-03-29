3699974cd08e9f84be4ccb7b139b6ff7aa9ab7ce
Станислав <stanislav9234@gmail.com>
2025-03-30 00:27:00 +0300

    fix

---
+++ Task8/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Смоделируйте работу простого калькулятора. Программа должна запрашивать 2 целых числа, а затем – код операции (например, 1 – сложение, 2 – вычитание, 3 – произведение, 4 – частное). После этого на консоль выводится ответ. Используйте обработку деления на ноль (DivideByZeroException), нечислового ввода (FormatException).
+            try
+            {
+                Console.WriteLine("Введите число a");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число b");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите код операции 1 – сложение, 2 – вычитание, 3 – произведение, 4 – частное");
+                int c = Convert.ToInt32(Console.ReadLine());
+                switch (c)
+                {
+                    case 1:
+                        Console.WriteLine(a + b);
+                        break;
+                    case 2:
+                        Console.WriteLine(a - b);
+                        break;
+                    case 3:
+                        Console.WriteLine(a * b);
+                        break;
+                    case 4:
+                        Console.WriteLine(a / b);
+                        break;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Введено не число - {ex.Message}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Деление на 0 - {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task8_2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            2) Напишите метод ValidateAge, проверяющий корректность возраста.Если возраст:
+            //•    < 0 - выбросить ArgumentException("Возраст не может быть отрицательным"),
+            //•    > 150 - выбросить ArgumentOutOfRangeException("Слишком большой возраст").
+
+            try
+            {
+                Console.WriteLine("Введите возраст");
+                int a = Convert.ToInt32(Console.ReadLine());
+                ValidateAge(a);
+                Console.WriteLine("Оптимальный возраст");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Некорректный возвраст - {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Проверка возраста завершена");
+            }
+            Console.ReadKey();
+
+        }
+
+        static void ValidateAge(int age)
+        {
+            if (age < 0)
+                throw new ArgumentException("Возраст не может быть отрицательным");
+            if (age > 150)
+                throw new ArgumentOutOfRangeException("Слишком большой возраст");
+        }
+    }
+}
