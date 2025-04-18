908cae6b08d47f9cf983f48fb2c00fda69f798a5
Станислав <stanislav9234@gmail.com>
2025-04-18 23:46:23 +0300

    fix

---
+++ Task11_1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[]
+            {
+            new Dog(),
+            new Cat(),
+            };
+            foreach (var animal in animals)
+            {
+                animal.ShowInfo();
+            }
+            Console.ReadKey();
+        }
+
+        public abstract class Animal
+        {
+            public abstract string Name { get; }
+            public abstract void Say();
+            public void ShowInfo()
+            {
+                Console.WriteLine(Name);
+                Say();
+            }
+        }
+        public class Dog : Animal
+        {
+            public override string Name => "Собака";
+            public override void Say()
+            {
+                Console.WriteLine("Гав!");
+            }
+        }
+        public class Cat : Animal
+        {
+            public override string Name => "Кошка";
+            public override void Say()
+            {
+                Console.WriteLine("Мяу!");
+            }
+        }
+    }
+}
---
+++ Task11_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable[] flyables = new IFlyable[]
+            {
+        new Bird(40),
+        new Airplane(100, 30)
+            };
+            foreach (var flyable in flyables)
+            {
+                flyable.Fly();
+            }
+            Console.ReadKey();
+        }
+        public interface IFlyable
+        {
+            void Fly();
+            int MaxAltitude { get; }
+        }
+        public class Bird : IFlyable
+        {
+            public int MaxAltitude { get; }
+            public Bird(int MaxAltitude)
+            {
+                this.MaxAltitude = MaxAltitude;
+            }
+            public void Fly()
+            {
+                Console.WriteLine($"Лечу на высоте {MaxAltitude} метров");
+            }
+        }
+        public class Airplane : IFlyable
+        {
+            public int CountPassengers { get; set; }
+            public int MaxAltitude { get; }
+            public Airplane(int maxAltitude, int countPassengers)
+            {
+                CountPassengers = countPassengers;
+                MaxAltitude = maxAltitude;
+            }
+            public void Fly()
+            {
+                Console.WriteLine($"Лечу на высоте {MaxAltitude} метров.Везу {CountPassengers} пассажиров");
+            }
+        }
+    }
+}
