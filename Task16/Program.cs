c783fe673f8c76314dc9c2d07636a192e11ccb77
Станислав <stanislav9234@gmail.com>
2025-06-18 21:01:21 +0300

    Task16 fix

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -13,7 +13,7 @@
             {
                 array[i] = random.Next(1, 10);
                 Console.Write($"{array[i]} ");
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
             }
 
             Console.WriteLine("\nМассив сгенерирован!");
@@ -22,19 +22,54 @@
 
         static int CalculateSumTask(int[] array)
         {
-            Console.WriteLine("Вычисление суммы...");
+            Console.WriteLine("Вычисление среднего арифметического...");
             int sum = 0;
 
             foreach (int num in array)
             {
                 sum += num;
-                Thread.Sleep(800);
+                Thread.Sleep(500);
             }
 
-            return sum/array.Length;
+            return sum / array.Length;
+        }
+        static async Task<int[]> GenerateArrayTaskAsync()
+        {
+            Console.WriteLine("Генерация массива методом async...");
+            int[] array = await Task.Run(() => GenerateArrayTask(8));
+            Console.WriteLine("Генерация массива методом async завершена");
+            return array;
+        }
+
+        static async Task<int> CalculateSumTaskAsync(int[] array)
+        {
+            Console.WriteLine("Вычисление среднего арифметического методом async...");
+            int result = await Task.Run(() => CalculateSumTask(array));
+            Console.WriteLine("Вычисление среднего арифметического методом async завершено");
+            return result;
         }
         static void Main(string[] args)
         {
+            //--------------------------Решение с Continuewith---------------------
+            //Task<int[]> task1 = Task.Run(() => GenerateArrayTask(9));
+            //Task<int> task2 = task1.ContinueWith(t =>
+            //{
+            //    int[] array = t.Result;
+            //    return CalculateSumTask(array);
+            //});
+            //Console.WriteLine($"Среднее арифметическое всех чисел в массиве: {task2.Result}");
+            //Console.ReadKey();
+
+            //-----------------------Решение с async/await-----------------------------
+            Console.WriteLine("Main запущен");
+            int[] array = GenerateArrayTaskAsync().Result;
+            int resultSum = CalculateSumTaskAsync(array).Result;
+            Console.WriteLine($"сгенерированный массив: {string.Join(", ", array)}");
+            Console.WriteLine($"Среднее арифметическое всех чисел в массиве: {resultSum}");
+            Console.WriteLine("Main завершен");
+            Console.ReadKey();
+
+
 
         }
     }
